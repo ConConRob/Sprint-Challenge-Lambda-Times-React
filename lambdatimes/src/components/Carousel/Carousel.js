ad410be383af9cb8b16c35a367ca1b5710c60746
conconrob <connortprobertson@gmail.com>
2019-02-22 10:20:49 +0000

    carousel with no end cond done

--- lambdatimes/src/components/Carousel/Carousel.js
+++ lambdatimes/src/components/Carousel/Carousel.js
@@ -5,29 +5,33 @@ export default class Carousel extends Component {
   constructor(props){
     super(props);
     this.state = {
-
+      currentPos: 0,
     }
   }
   componentDidMount(){
-
+    
   }
 
   leftClick = () => {
-
+    this.setState(curState => ({currentPos: curState.currentPos-1}))
   }
 
   rightClick = () => {
-
+    this.setState(curState => ({currentPos: curState.currentPos+1}))
   }
 
-  selectedImage = () => {
-    return <img src={} style={{display: 'block'}} />
-  }
+  // selectedImage = () => {
+  //   return <img src={} style={{display: 'block'}} />
+  // }
   
   render(){
     return (
       <div className="carousel">
         <div className="left-button" onClick={this.leftClick}>{"<"}</div>
+        <img 
+          src={carouselData[this.state.currentPos]} 
+          alt="carousel"
+        />
         <div className="right-button" onClick={this.rightClick}>{">"}</div>
       </div>
     )
--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Tabs from './Tabs';
 import Cards from './Cards';
-
+import Carousel from '../Carousel/Carousel'
 // Importing our tab and card data. No need to change anything here.
 import { tabData, cardData } from '../../data';
 
@@ -56,6 +56,7 @@ export default class Content extends Component {
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
         */}
+        <Carousel />
         <Tabs 
           tabs={this.state.tabs} 
           selectedTab={this.state.selected}
