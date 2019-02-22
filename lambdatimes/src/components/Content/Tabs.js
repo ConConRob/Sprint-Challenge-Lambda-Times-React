fb10df91c6d488377ece7ec6e6ec88fd1b2e04c8
conconrob <connortprobertson@gmail.com>
2019-02-22 08:52:00 +0000

    ADDED css to tab and tabs

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -2,10 +2,36 @@ import React from 'react';
 import PT from 'prop-types';
 import styled from 'styled-components';
 import Tab from './Tab';
+
+const StyledTabs = styled.div`
+    border-bottom: 1px solid lightgrey;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+    width: 100%;
+    height: 47px;
+    background-color: #fff;
+  @media (min-width: 1280px) {
+    width: 1280px;
+  }
+  .topics {
+    display: flex;
+    justify-content: none;
+    align-items: center;
+    flex-direction: row;
+  }
+  .title {
+    font-size: 12px;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+
+`
 const Tabs = props => {
   const {tabs} = props
   return (
-    <div className="tabs">
+    <StyledTabs>
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
@@ -13,7 +39,7 @@ const Tabs = props => {
           tabs.map((tab, index) => <Tab key={index} tab={tab} />)
         }
       </div>
-    </div>
+    </StyledTabs>
   );
 };
 
