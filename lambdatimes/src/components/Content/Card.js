0e3f1b9774de2a0b1bf40d814c1a9526468e76e2
conconrob <connortprobertson@gmail.com>
2019-02-22 09:06:02 +0000

    DONE style for cards

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -2,6 +2,18 @@ import React from 'react';
 import PT from 'prop-types';
 import styled from 'styled-components';
 
+const StyledCard = styled.div`
+  border-bottom: 1px solid lightgrey;
+  display: flex;
+  justify-content: space-between;
+  align-items: none;
+  flex-direction: column;
+  background-color: #fff;
+  width: 380px;
+  margin-bottom: 16px;
+  padding: 24px;
+` 
+
 const Card = props => {
   const {card} = props
   return (
